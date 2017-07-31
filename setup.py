771dea24e15836308cb074c9eb42fab84a993ae3
Calvin Xu <1020806611@qq.com>
2017-07-31 18:54:14 +0800

    Update

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ except ImportError:
 config = {
     'description': 'Morse code encoder & decoder',
     'author': 'Calvin Xu',
-    'url': 'URL to get it at.',
-    'download_url': 'Where to download it.',
+    'url': 'https://github.com/Calvin-Xu/MorsePy',
+    'download_url': 'https://github.com/Calvin-Xu/MorsePy',
     'author_email': 'calvinxu806@vip.163.com',
     'version': '1.0.0',
     'install_requires': ['pytest'],
